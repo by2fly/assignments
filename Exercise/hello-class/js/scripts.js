ed939ffd5d9919b3b6fafb9ab667a731cd15013c
Brian Young <by2fly@gmail.com>
2019-07-11 13:41:13 -0600

    the If Statement Olympics

--- Exercise/hello-class/js/scripts.js
+++ Exercise/hello-class/js/scripts.js
@@ -1,7 +1,30 @@
-
-var salutation = 'hi'
-
-function sayHello(greeting){
-return sayHello
+var sayHello = function(){
+    console.log("hello class")
 }
-console.log(sayHello("greeting"))
+
+//call the function
+
+sayHello "hello class"
+
+//•	change this function expression to a function declaration
+
+
+//•	Give the function a name parameter
+
+
+//•	Pass the function call a string (preferably a name) as an argument.
+
+
+//•	Return a cool string, like: "Hello Fred!"!
+
+
+
+
+//•	Give the function a age parameter
+
+
+
+//•	Pass the function call a string (preferably a age) as an argument.
+
+
+//•	Return a cool string, like: "Hello Fred, how do you like being 27?"
